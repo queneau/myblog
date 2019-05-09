606fa33930aea1249a9bf8cc490cc2f92c886a92
kuno <risakno3@gmail.com>
2019-05-09 21:48:53 +0900

    fix settings.py HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#!bk(n$n71pao*td!47ab4e8@k*r-x^--m8a(p$d&=))(tarxk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com', 'kno.pythonanywhere.com']
 
 
 # Application definition
